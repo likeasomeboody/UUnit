0abde469d0a90d203703381d67ef32bdfa2bc34b
Luke Sawyers <luke_sawyers@hotmail.co.nz>
2018-05-08 10:12:56 +1200

    Add UUnit Unity project content

---
+++ Assets/UUnit/Scripts/UUnitEditorTestRunner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UUnit.Unity.Editor
+{
+    public class UUnitEditorTestRunner : EditorWindow
+    {
+        #region Static
+
+        [MenuItem("UTest/Test Runner")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(UUnitEditorTestRunner));
+        }
+
+        #endregion
+
+        #region Fields
+
+
+
+        #endregion
+
+        #region Methods
+
+        #region Methods.Behaviours
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnGUI()
+        {
+
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
